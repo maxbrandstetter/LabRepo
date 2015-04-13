f681664aa0377f2b8ee7836ed589ff068d677158
Max Brandstetter <middlestetter999@gmail.com>
2015-04-12 21:12:05 -0700

    Updated square_or_rectangle files for tabbing consistency

--- source/square_or_rectangle.py
+++ source/square_or_rectangle.py
@@ -1,34 +1,34 @@
 """
-The following code determine if a set of 4 values is a square or a rectangle
+The following code determines if a set of 4 values is a square or a rectangle.
 """
 
 def determine_square_or_rectangle(a=0, b=0, c=0, d=0):
-        """
-        Determine if the given sides form a square or rectangle
+    """
+    Determine if the given sides form a square or rectangle
 
-        :param a: line a
-        :type a: float or int
+    :param a: line a
+    :type a: float or int
 
-        :param b: line b
-        :type b: float or int
-        
-        :param c: line c
-        :type c: float or int
-        
-        :param d: line d
-        :type d: float or int
+    :param b: line b
+    :type b: float or int
 
-        :return: "square", "rectangle", or "invalid"
-        :rtype: str
-        """
-	if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(d, (int, float))):
-		return "invalid"	
-	
-	if a <= 0 or b <= 0 or c <= 0 or d <= 0:
-		return "invalid"
-	
-	if a == b and b == c and c == d:
-		return "square"
-		
-	if a == b and c == d or a == c and b == d or a == d and b == c:
-		return "rectangle"
+    :param c: line c
+    :type c: float or int
+
+    :param d: line d
+    :type d: float or int
+
+    :return: "square", "rectangle", or "invalid"
+    :rtype: str
+    """
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(d, (int, float))):
+        return "invalid"
+
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        return "invalid"
+
+    if a == b and b == c and c == d:
+        return "square"
+
+    if a == b and c == d or a == c and b == d or a == d and b == c:
+        return "rectangle"
