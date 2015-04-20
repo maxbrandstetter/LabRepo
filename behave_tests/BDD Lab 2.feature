ad1a48cf114aab0de2d6fd3f73a490a1251e2ed3
Max Brandstetter <middlestetter999@gmail.com>
2015-04-20 11:33:36 -0700

    Additional progress on Round 1 BDD

--- behave_tests/BDD Lab 2.feature
+++ behave_tests/BDD Lab 2.feature
@@ -5,4 +5,22 @@ Feature: BDD What is Faster
     When a file name is entered
     Then the cities, distances, and speeds will be read
 
-  Scenario:
+  Scenario: Select Speed
+    Given the user is researching speeds
+    When an estimated speed is selected
+    Then the user can see which would be faster
+
+  Scenario: Select HD Size
+    Given the user is researching speeds
+    When a hard drive size is selected
+    Then the user has more data
+
+  Scenario: Is HD or Network Faster
+    When the user enters information
+    Then the user can see which would be faster
+
+  Scenario: Difference in Time
+    When the user enters information
+    Then the user can see the time difference
+
+
---
+++ behave_tests/steps/BDD_Steps.py
@@ -0,0 +1,41 @@
+from behave import given, when, then
+
+@Given('the user is researching speeds')
+def step_impl(context):
+    def prepare_for_input():
+
+@When('a file name is entered')
+def step_impl(context):
+    def accept_input():
+
+@Then('the cities, distances, and speeds will be read')
+def step_impl(context):
+    def read_file():
+
+@When('an estimated speed is selected')
+def step_impl(context):
+    def accept_input():
+
+@Then('the user can see which would be faster')
+def step_impl(context):
+    def compare_options():
+
+@When('a hard drive size is selected')
+def step_impl(context):
+    def accept_input():
+
+@Then('the user has more data')
+def step_impl(context):
+    def total_data():
+
+@When('the user enters information')
+def step_impl(context):
+    def accept_input():
+
+@Then('the user can see which would be faster')
+def step_impl(context):
+    def compare_options():
+
+@Then('the user can see the time difference')
+def step_impl(context):
+    def get_time_difference():
