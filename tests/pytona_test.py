ec49903915fa18c5ec157b17c7efd92f15f8057f
Max Brandstetter <middlestetter999@gmail.com>
2015-05-02 13:46:56 -0700

    Updated tests

--- tests/pytona_test.py
+++ tests/pytona_test.py
@@ -4,6 +4,7 @@ Tests for pyTona, as per the requirements listed.
 from pyTona.main import Interface
 from unittest import TestCase
 from ReqTracer import requirements
+import getpass
 
 
 class TestPyTonaFunctions(TestCase):
@@ -31,4 +32,31 @@ class TestPyTonaFunctions(TestCase):
     def test_no_question_mark(self):
         test_interface = Interface()
         self.assertEqual(test_interface.ask('How many seconds since|'), 'Was that a question?')
+
+    @requirements(['#0008'])
+    def test_valid_match(self):
+        test_interface = Interface()
+        self.assertEqual(test_interface.ask('Why don\'t you shutdown>'), "I'm afraid I can't do that {0}".format(getpass.getuser()))
+
+    @requirements(['#0009'])
+    def test_invalid_match(self):
+        test_interface = Interface()
+        self.assertEqual(test_interface.ask('How does this work>'), "I don't know, please provide the answer")
+
+    @requirements(['#0010'])
+    def test_previous_question(self):
+        test_interface = Interface()
+        test_interface.ask('How many seconds since>')
+        self.assertEqual(test_interface.last_question, 'How many seconds since')
+
+    @requirements(['#0012'])
+    def test_no_previous_questions(self):
+        test_interface = Interface()
+        self.assertEqual(test_interface.teach(), 'Please ask a question first')
+
+    @requirements(['#0013'])
+    def test_existing_answer(self):
+        test_interface = Interface()
+        test_interface.ask('How many seconds since>')
+        self.assertEqual(test_interface.teach('A lot'), 'I don\'t know about that. I was taught differently')
 
