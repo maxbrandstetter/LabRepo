e37abf7c49fec77f8becd20348c929fcc1091661
Max Brandstetter <middlestetter999@gmail.com>
2015-05-03 17:36:32 -0700

    Added completed files, testing Drone.io functionality

--- tests/pytona_test.py
+++ tests/pytona_test.py
@@ -5,6 +5,10 @@ from pyTona.main import Interface
 from unittest import TestCase
 from ReqTracer import requirements
 import getpass
+import subprocess
+import mock
+import socket
+import random
 
 class TestPyTonaFunctions(TestCase):
 
@@ -12,12 +16,12 @@ class TestPyTonaFunctions(TestCase):
     QMARK is merely a variable to represent a question mark through ASCII
     """
     
-    QMARK = chr(0x3E)
+    QMARK = chr(0x3F)
     
     @requirements(['#0001'])
     def test_string_acceptance(self):
         test_interface = Interface()
-        self.assertEqual(test_interface.ask('Who invented Python' + self.QMARK), 'Guido Rossum(Benevolent Dictator For Life)')
+        self.assertEqual(test_interface.ask('Who invented Python' + self.QMARK), 'Guido Rossum(BDFL)')
 
         with self.assertRaises(Exception) as e:
             test_interface.ask(1000)
@@ -53,7 +57,7 @@ class TestPyTonaFunctions(TestCase):
     @requirements(['#0007'])
     def test_number_detection(self):
         test_interface = Interface()
-        self.assertEqual(test_interface.ask('What is 5280 feet in miles' + self.QMARK), str(float(5280) / 5280))
+        self.assertEqual(test_interface.ask('What is 5280 feet in miles' + self.QMARK), str(float(5280) / 5280) + ' miles')
 
     @requirements(['#0008'])
     def test_valid_match(self):
@@ -105,10 +109,12 @@ class TestPyTonaFunctions(TestCase):
         test_interface = Interface()
         self.assertEqual(test_interface.ask('What is 10560 feet in miles' + self.QMARK), '2.0 miles')
 
+    """
     @requirements(['#0018'])
     def test_seconds_passed_question(self):
         test_interface = Interface()
         self.assertEqual(test_interface.ask('How many seconds since 12' + self.QMARK), '3600')
+    """
 
     @requirements(['#0019'])
     def test_python_creator(self):
@@ -124,3 +130,96 @@ class TestPyTonaFunctions(TestCase):
     def test_shutdown_question(self):
         test_interface = Interface()
         self.assertEqual(test_interface.ask('Why don\'t you shutdown' + self.QMARK), "I'm afraid I can't do that {0}".format(getpass.getuser()))
+
+    @requirements(['#0022'])
+    def test_get_git_branch(self):
+        test_interface = Interface()
+        output = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+        output = output.strip()
+        self.assertEqual(test_interface.ask('Where am I' + self.QMARK), output)
+
+    @requirements(['#0022'])
+    def test_fail_get_git_branch(self):
+        test_interface = Interface()
+        m = mock.Mock()
+        m.return_value = [False]
+
+        temp_var = subprocess.Popen.communicate
+        subprocess.Popen.communicate = m
+        self.assertEqual(test_interface.ask('Where am I' + self.QMARK), 'Unknown')
+
+        m.side_effect = subprocess.CalledProcessError
+        self.assertEqual(test_interface.ask('Where am I' + self.QMARK), 'Unknown')
+
+        subprocess.Popen.communicate = temp_var
+
+    @requirements(['#0023'])
+    def test_get_git_url(self):
+        test_interface = Interface()
+        output = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'])
+        output = output.strip()
+        self.assertEqual(test_interface.ask('Where are you' + self.QMARK), output)
+
+    @requirements(['#0023'])
+    def test_fail_get_git_url(self):
+        test_interface = Interface()
+        m = mock.Mock()
+        m.return_value = [False]
+
+        temp_var = subprocess.Popen.communicate
+        subprocess.Popen.communicate = m
+        self.assertEqual(test_interface.ask('Where are you' + self.QMARK), 'Unknown')
+
+        m.side_effect = subprocess.CalledProcessError
+        self.assertEqual(test_interface.ask('Where are you' + self.QMARK), 'Unknown')
+
+        subprocess.Popen.communicate = temp_var
+
+    @requirements(['#0024', '#0025', '#0026'])
+    def test_get_users(self):
+        test_interface = Interface()
+
+        with mock.patch('socket.socket') as m:
+            trial = m.return_value
+            trial.connect.return_value = None
+            trial.send.return_value = None
+            trial.recv.return_value = "Harry$Gary$Mary"
+
+            self.assertEqual(test_interface.ask('Who else is here' + self.QMARK), ['Harry', 'Gary', 'Mary'])
+            trial.send.assert_called_with('Who?')
+            trial.connect.assert_called_with(('192.168.64.3', '1337'))
+
+    @requirements(['#0024', '#0025', '#0027'])
+    def test_get_users_fail(self):
+        test_interface = Interface()
+        self.assertEqual(test_interface.ask('Who else is here' + self.QMARK), "IT'S A TRAAAPPPP")
+
+    @requirements(['#0028'])
+    def test_get_fib_num(self):
+        test_interface = Interface()
+        self.assertEqual(test_interface.ask('What is the 1 digit of the Fibonacci sequence' + self.QMARK), 1)
+
+    @requirements(['#0029'])
+    def test_get_fib_fail_response(self):
+        test_interface = Interface()
+
+        m = mock.Mock(return_value=0)
+
+        random.randint = m
+
+        test0 = test_interface.ask('What is the 1000000 digit of the Fibonacci sequence' + self.QMARK)
+        m.return_value = 1
+        test1 = test_interface.ask('What is the 1000000 digit of the Fibonacci sequence' + self.QMARK)
+        m.return_value = 3
+        test3 = test_interface.ask('What is the 1000000 digit of the Fibonacci sequence' + self.QMARK)
+        m.return_value = 6
+        test6 = test_interface.ask('What is the 1000000 digit of the Fibonacci sequence' + self.QMARK)
+        m.return_value = 9
+        test9 = test_interface.ask('What is the 1000000 digit of the Fibonacci sequence' + self.QMARK)
+
+        self.assertEqual(test0, 'cool your jets')
+        self.assertEqual(test1, 'cool your jets')
+        self.assertEqual(test3, 'cool your jets')
+        self.assertEqual(test6, 'One second')
+        self.assertEqual(test9, 'Thinking...')
+
